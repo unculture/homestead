ca5046f067b1c8cae81fe36225ae4c90b55619a3
James Browne <jb@jamesbrowne.me>
2014-12-18 15:47:23 +0000

    Fixing a ruby error

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -49,7 +49,7 @@ class Homestead
           if (site.has_key?("hhvm") && site["hhvm"])
             s.inline = "bash /vagrant/scripts/serve-hhvm.sh $1 $2"
             s.args = [site["map"], site["to"]]
-          else if (site.has_key?("silverstripe") && site["silverstripe"])
+          elseif (site.has_key?("silverstripe") && site["silverstripe"])
             s.inline = "bash /vagrant/scripts/serve-silverstripe.sh $1 $2"
             s.args = [site["map"], site["to"]]
           else
